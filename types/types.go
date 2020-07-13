77f1a7de473607f61f8606b13d715c30cdb42dcf
Nicholas St. Germain <nick@cajun.pro>
2020-07-12 20:11:09 -0500

    * Added notation for everything in types.go * Renamed Time to UnixTimestamp for explicitness * Added Scan() for UnixTimestamp

--- types/types.go
+++ types/types.go
@@ -9,8 +9,12 @@ import (
 	"time"
 )
 
+// ConvertibleBoolean is a custom boolean struct used for manipulating
+// booleans of string, integer, and boolean to/from JSON and SQL
 type ConvertibleBoolean bool
 
+// UnmarshalJSON re-implements the encoding/json Unmarshal method.
+// It will cast the any of the truthy/falsy variants to boolean
 func (bit *ConvertibleBoolean) UnmarshalJSON(data []byte) error {
 	asString := string(data)
 	if asString == "1" || asString == "true" {
@@ -23,6 +27,9 @@ func (bit *ConvertibleBoolean) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// Scan re-implements the database/sql Scan() method.
+// It will map the integer found in the response to
+// its boolean equivalent
 func (bit *ConvertibleBoolean) Scan(src interface{}) error {
 	switch src.(type) {
 	case int64:
@@ -38,25 +45,56 @@ func (bit *ConvertibleBoolean) Scan(src interface{}) error {
 	return nil
 }
 
-type Time time.Time
+// UnixTimestamp is a custom time.Time struct used for manipulating
+// unix timestamps to/from JSON, XML, and SQL
+type UnixTimestamp time.Time
 
-func (t Time) MarshalJSON() ([]byte, error) {
+// MarshalJSON re-implements the encoding/json Marshal method.
+// It will return a quote-enclosed RFC3339 string as []byte
+func (t UnixTimestamp) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf("\"%s\"", t.String())), nil
 }
 
-func (t *Time) UnmarshalXMLAttr(attr xml.Attr) error {
+// UnmarshalXMLAttr re-implements the database/xml Unmarshal()
+// method for attributes. It will map the unix timestamp integer
+// found in the response to a UnixTimestamp struct
+func (t *UnixTimestamp) UnmarshalXMLAttr(attr xml.Attr) error {
 	n, err := strconv.ParseInt(attr.Value, 10, 64)
 	if err != nil {
 		return err
 	}
-	*t = Time(time.Unix(n, 0))
+	*t = UnixTimestamp(time.Unix(n, 0))
 	return nil
 }
 
-func (t *Time) String() string {
-	return time.Time(*t).Format(time.RFC3339)
+// String is a convenience function to get a string
+// response based on format. time.RFC3339 is the default
+func (t *UnixTimestamp) String(format ...string) string {
+	f := time.RFC3339
+	if len(format) > 0 {
+		f = format[0]
+	}
+	return time.Time(*t).Format(f)
 }
 
-func (t Time) Value() (driver.Value, error) {
+// Scan re-implements the database/sql Scan() method.
+// It will convert the RFC 3339 string found in the
+// response to a UnixTimestamp struct
+func (t *UnixTimestamp) Scan(src interface{}) error {
+	switch src.(type) {
+	case string:
+		if v, err := time.Parse(time.RFC3339, src.(string)); err != nil {
+			return err
+		} else {
+			*t = UnixTimestamp(v)
+		}
+	}
+	return nil
+}
+
+// Value re-implements the database/sql Value() method.
+// It will convert the custom struct to the standard
+// time.Time struct
+func (t UnixTimestamp) Value() (driver.Value, error) {
 	return time.Time(t), nil
 }
