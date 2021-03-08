6d325b3028c3366602f757d216e08547757ec607
Nicholas St. Germain <nick@cajun.pro>
2021-03-08 13:24:36 -0600

    fix quoting issue with IntString and add MarshalJSON()

--- types/types.go
+++ types/types.go
@@ -107,10 +107,21 @@ type IntString int
 // UnmarshalJSON re-implements the encoding/json Unmarshal method.
 // It will cast the string to int type
 func (i *IntString) UnmarshalJSON(data []byte) error {
-	integer, err := strconv.Atoi(string(data))
+	unquote, err := strconv.Unquote(string(data))
+	if err != nil {
+		return err
+	}
+	var integer int
+	integer, err = strconv.Atoi(unquote)
 	if err != nil {
 		return err
 	}
 	*i = IntString(integer)
 	return nil
+}
+
+// MarshalJSON re-implements the encoding/json Marshal method.
+// It will return a quote-enclosed int as []byte
+func (i IntString) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"%d\"", i)), nil
 }
